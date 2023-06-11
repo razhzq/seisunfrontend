137deff7073888cafa968f57da4e444cfb23c25c
bluesky0115 <gold.star.bee@gmail.com>
2023-06-11 07:34:57 -0700

    fixed assetsitem pixel perfect

--- src/components/AssetsItem.js
+++ src/components/AssetsItem.js
@@ -5,24 +5,42 @@ import chart1 from '../assets/chart1.png';
 const AssetsItem = ({name, CHG, VOL, }) => {
 	const [ isFlip, setIsFlip ] = useState(false);
   return (
-		<div className='!mx-[10px] flip-card !m-auto !w-[342px] h-[219px] bg-white border border-white rounded-[6px] !bg-[#141217] p-[10px] relative border '>
+		<div className='!mx-[10px] flip-card !m-auto !w-[342px] h-[219px] bg-white border-white rounded-[6px] !bg-[#141217] p-[10px] relative border '>
 			<div className={`flip-card-inner ${isFlip ? 'card-rotate': ''}`} onMouseEnter={() => setIsFlip(!isFlip)} onMouseLeave={() => setIsFlip(!isFlip)}>
 				<div className="flip-card-front">
 					<div className="flex justify-between font-black text-white text-[34px] leading-[34px] !mr-0">
 						<img src={chart} className='inline-block'></img>
-						<p className='flex items-center uppercase'>{name}</p>
+						<p className='flex items-center uppercase font-[900] text-[34px] leading-[34px] uppercase'>{name}</p>
 					</div>
 					<div className='!w-full h-[157px] absolute bottom-0 left-0 right-0 !mr-0'>
 						<img src={chart1} className="w-full"></img>
 
 						<div className='absolute bottom-[8px] flex justify-between left-0 right-0'>
 							<div>
-								<p className="text-white text-opacity-80 text-left ml-[15px] ">CHG: +13%</p>
-								<p className="text-white text-opacity-80 ml-[15px] ">VOL: $2,321,2323</p>
+								<div className="flex ml-[5px]">
+									<div>
+										<p className="text-white text-opacity-50 text-[10px] leading-[34px] text-left font-[400] uppercase">CHG:</p>
+									</div>
+									
+									<div className="flex items-center">
+										<div className="w-0 h-0 border-[5px] border-t-[#FF0000] border-l-transparent border-r-transparent border-b-transparent mt-[5px] ml-[15px]"></div>
+										<p className="text-[red] uppercase text-[18px] font-[400] leading-[34px]">13%</p>
+									</div>
+								</div>
+
+								<div className="flex ml-[5px]">
+									<div>
+										<div>
+											<p className="text-white text-opacity-50 text-[10px] leading-[34px] text-left font-[400px] uppercase">VOL:</p>
+										</div>
+									</div>
+
+									<p className="text-white uppercase text-[15px] font-[400px] leading-[34px] ml-[15px]">$2,123,132</p>
+								</div>
 							</div>
 
 							<div className="flex items-center">
-								<p className="text-white mr-[15px] ">PAIRS...</p>
+								<p className="text-white mr-[15px] leading-[34px] text-[14px]">PAIRS...</p>
 							</div>
 						</div>
 					</div>
