a8fe746900d3a9c431bbb03b95e1d3e4f48c04c7
bluesky0115 <gold.star.bee@gmail.com>
2023-06-11 19:30:27 -0700

    fixed height of tradeview

--- src/views/AssetsView.js
+++ src/views/AssetsView.js
@@ -46,20 +46,20 @@ const AssetsView = () => {
   };
 
   return (
-      <div className="container assets flex flex-col py-[190px] mx-auto bg-black h-screen max-h-screen">
-      <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase ">Discover<br/>Tradable Assets</p>
+      <div className="container assets flex flex-col py-[190px] mx-auto bg-black h-screen">
+        <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase ">Discover<br/>Tradable Assets</p>
 
-      <div className="border border-white w-[327px] border-opacity-50 mx-auto mt-[33px]"></div>
+        <div className="border border-white w-[327px] border-opacity-50 mx-auto mt-[33px]"></div>
 
-      <section className="center slider mt-[19px]">
-        <Slider {...settings}>
-          <AssetsItem name="crypto"></AssetsItem>
-          <AssetsItem name="forex"></AssetsItem>
-          <AssetsItem name="stocks"></AssetsItem>
-          <AssetsItem name="defi"></AssetsItem>
-        </Slider>
-      </section>
-    </div>
+        <section className="center slider mt-[19px]">
+          <Slider {...settings}>
+            <AssetsItem name="crypto"></AssetsItem>
+            <AssetsItem name="forex"></AssetsItem>
+            <AssetsItem name="stocks"></AssetsItem>
+            <AssetsItem name="defi"></AssetsItem>
+          </Slider>
+        </section>
+      </div>
   );
 }
 
