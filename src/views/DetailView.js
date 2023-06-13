70e6e063aa86481ef46a95fb01da86d86bdc2612
bluesky0115 <gold.star.bee@gmail.com>
2023-06-12 19:04:25 -0700

    complete the landing page

--- src/views/DetailView.js
+++ src/views/DetailView.js
@@ -7,9 +7,9 @@ import trees_right from '../assets/BondingCurve/trees_right.png';
 const DetailView = () => {
 	return (
 		<div className="bg-white mx-auto">
-			<div className="container mx-auto flex flex-col lg:flex-row h-[1391px] md:h-[974px]" >
+			<div className="container mx-auto flex flex-col xl:flex-row h-[1391px] zl:h-[974px]" >
 				<div className="flex-1 relative overflow-hidden">
-					<div className='absolute left-[20px] lg:left-[124px]  top-[87px] w-[403px] text-left z-10'>
+					<div className='absolute left-[20px] xl:left-[124px]  top-[87px] w-[403px] text-left z-10'>
 						<p className='uppercase font-bold text-black text-[45px] leading-[34px]'>BONDING CURVE</p>
 						<p className='w-[365px] ml-[38px] mt-[13px] font-normal font-medium text-[20px] leading-[34px]'>Self-regulating mechanism for autonomous vault rebalancing to ensure price stability. Adjusts token supply in response to changes in protocol demand to accurately reflect token utility.</p>
 
@@ -24,20 +24,20 @@ const DetailView = () => {
 						<img src={vines_left}></img>
 					</div>
 
-					<div className="left-[-144px] top-[33px] absolute w-[737px] visible xl:hidden">
+					<div className="left-[-224px] top-[133px] absolute w-[737px] visible xl:hidden">
 					<img src={vines_right}></img>
 					</div>
 				</div>
 
 				<div className="flex-1 relative overflow-hidden">
-					<div className='absolute right-[107px] top-[95px] w-[383px] text-left z-10'>
+					<div className='absolute right-20 xl:right-[107px] top-0 xl:top-[95px] w-[383px] text-left z-10'>
 						<p className='uppercase font-bold text-black text-[45px] leading-[34px]'>PRICING NETWORK</p>
 						<p className='w-[365px] text-left ml-[15px] mt-[19px] font-normal font-medium text-[20px] leading-[34px]'>Node operators creating consensus for on-chain pricing data for use by the community.</p>
 
 						<button className="bg-[#1A1A1A] w-[105px] h-[32px] rounded-[15px] leading-[34px] text-[12px] text-center uppercase text-white mx-auto flex justify-center mt-[19px] font-bold items-center">LEARN MORE</button>
 
 					</div>
-					<div className="right-[-4px] bottom-0 absolute w-[770px]">
+					<div className="right-[-250px] xl:right-[-4px] bottom-0 absolute w-[770px]">
 						<img src={trees_right}></img>
 					</div>
 
