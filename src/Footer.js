b296a14a4a2deb49b16cacd9722605a126c7c67f
bluesky0115 <gold.star.bee@gmail.com>
2023-06-18 16:57:51 -0700

    changed footer image on mible & desktop

--- src/Footer.js
+++ src/Footer.js
@@ -6,18 +6,24 @@ import twitter from './assets/socials/twitter-logo.png';
 
 const Footer = () => {
 	return (
-		<div className="bg-[url('./assets/main_bg.png')] bg-cover bg-center h-[822px] flex flex-col justify-center items-center h-screen">
-			<div>
-				<div><img src={transaped} className='mx-auto' alt=''></img></div>
-				<div className='flex flex-row gap-[20px] items-center mt-[29px]'>
-					<a href="#"><img src={medium} alt=''></img></a>
-					<a href="https://discord.com/invite/FbFjCz4PAR"><img src={discord} alt=''></img></a>
-					<a href="#"><img src={twitter} alt=''></img></a>
-				</div>
+		<div className="bg-[url('./assets/footer_mobile.png')] md:bg-[url('./assets/footer.png')] bg-cover bg-center h-[822px] flex flex-col justify-center items-center h-screen relative">
+
+			<div className='bg-[#000000] bg-opacity-[0.35] top-0 left-0 right-0 bottom-0 absolute'>
+
 			</div>
-			<div className="flex flex-row justify-between w-[208px] mt-[6px]">
-				<a href="/docs" className="text-white text-[18px] leading-[22px] font-[800] uppercase flex-none order-none grow-0">DOCS</a>
-				<a href="/contacts" className="text-white text-[18px] leading-[22px] font-[800] uppercase flex-none order-none grow-0">contact us</a>
+			<div className='z-[50]'>
+				<div><img src={transaped} className='mx-auto' alt=''></img></div>
+				<div className='flex flex-row items-center mt-[25px]'>
+					<a href="#"><img src={medium} alt=''></img></a>
+					<a href="https://discord.com/invite/FbFjCz4PAR" className='ml-[20px]'><img src={discord} alt=''></img></a>
+					<a href="#"><img src={twitter} alt='' className='ml-[23px]'></img></a>
+				</div>
+
+				<div className="flex flex-row justify-between w-[208px] mt-[6px]">
+					<a href="/docs" className="text-white text-[18px] leading-[22px] font-[800] uppercase flex-none order-none grow-0">DOCS</a>
+					<a href="/contacts" className="text-white text-[18px] leading-[22px] font-[800] uppercase flex-none order-none grow-0">contact us</a>
+				</div>
+				
 			</div>
 		</div>
 	)
