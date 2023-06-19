6867a188bc4feb9b1fa9141f3e4a4895eef22783
bluesky0115 <gold.star.bee@gmail.com>
2023-06-19 12:03:25 -0700

    complete detailview

--- src/views/DetailView.js
+++ src/views/DetailView.js
@@ -54,7 +54,7 @@ const DetailView = () => {
 
 			<div className='block xl:hidden bg-white flex justify-center relative overflow-hidden'>
 				<div className="overflow-hidden h-[700px] w-[428px]">
-					<div className='mt-[137px] w-[365px] text-left text-center ml-[30px]'>
+					<div className='mt-[62px] w-[365px] text-left text-center ml-[30px]'>
 						<p className='uppercase font-bold text-black text-[45px] leading-[34px] text-left'>BONDING CURVE</p>
 						<p className='w-full h-[187px] mt-[13px] font-[400] text-[20px] leading-[34px] text-left'>Self-regulating mechanism for autonomous vault rebalancing to ensure price stability. Adjusts token supply in response to changes in protocol demand to accurately reflect token utility.</p>
 
@@ -70,15 +70,15 @@ const DetailView = () => {
 			</div>
 
 			<div className='block xl:hidden bg-white flex justify-center  overflow-hidden'>
-				<div className="overflow-hidden h-[700px] relative">
-					<div className='mt-[137px] w-[403px] text-left z-10 text-center  h-[560px] ml-[30px]'>
+				<div className="overflow-hidden h-[570px] relative">
+					<div className='mt-[30px] w-[403px] text-left z-10 text-center  h-[570px] ml-[30px]'>
 						<p className='uppercase font-bold text-black text-[45px] leading-[34px] text-left'>PRICING NETWORK</p>
 						<p className='w-[365px] h-[120px] mt-[19px] font-normal font-medium text-[20px] leading-[34px] text-left'>Node operators creating consensus for on-chain pricing data for use by the community.</p>
 
 						<button className="bg-[#1A1A1A] w-[167px] h-[51px] rounded-[15px] leading-[34px] text-[18px] text-center uppercase text-white ml-[79px] flex justify-center font-bold items-center mt-[50px]">LEARN MORE</button>						
 					</div>
 
-					<div className="right-[-156px] bottom-[0px] absolute w-[543px] h-[440px]">
+					<div className="right-[-176px] top-[130px] absolute w-[543px] h-[440px]">
 						<img src={trees_bottom} alt=''></img>
 					</div>
 				</div>
