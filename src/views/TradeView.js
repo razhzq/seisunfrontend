09b5f7109e7c00f6ae03853ef9d8a0f0b0af4b9c
bluesky0115 <gold.star.bee@gmail.com>
2023-06-27 03:21:48 -0700

    change header footer svg

--- src/views/TradeView.js
+++ src/views/TradeView.js
@@ -4,7 +4,7 @@ import LogoView from './LogoView';
 const TradeView = () => {
   return (
 		<div className='tradeview bg-black h-screen md:h-fit'>
-			<div className="bg-[url('./assets/main_bg.png')] bg-cover bg-center h-screen flex justify-center items-center relative ">
+			<div className="bg-[url('./assets/main_bg.svg')] bg-cover bg-center h-screen flex justify-center items-center relative ">
 				<div className="flex justify-center !flex-col items-center">
 					<p className="border-[#AEAEAE] border-opacity-80 rounded-[10px] text-white font-bold text-[55px] leading-[55px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] uppercase">THE aped ecosystem</p>
 
