fa5ddb21cc0635ba8208442fa3dc6957163936a7
bluesky0115 <gold.star.bee@gmail.com>
2023-06-28 22:14:54 -0700

    redesign the assetsitem

--- src/components/AssetsItem.js
+++ src/components/AssetsItem.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect} from 'react';
 import Pattern from '../assets/pattern.png';
 import Chart from '../components/ChartItem';
 import ReactCardFlip from "react-card-flip";
-import btc from '../assets/crypto/btc.webp';
-import sol from '../assets/crypto/sol.png';
-import busd from '../assets/crypto/busd.png';
-import eth from '../assets/crypto/eth.webp';
+
+import BTC from '../assets/crypto/btc.webp';
+import SOL from '../assets/crypto/sol.png';
+import BUSD from '../assets/crypto/busd.png';
+import ETH from '../assets/crypto/eth.webp';
 
 import EUR from '../assets/forex/EU.svg';
 import GBP from '../assets/forex/GB.svg';
@@ -13,16 +14,23 @@ import CNH from '../assets/forex/CN.svg';
 import AUD from '../assets/forex/AU.svg';
 const AssetsItem = (props) => {
 	const [ isFlip, setIsFlip ] = useState(false);
-	const chg = props.dailyChange;
-	
-	const valueRange = Math.max(...chg) - Math.min(...chg);
-	const normalizedValues = chg.map(value => (value - Math.min(...chg)) / valueRange);
-	const factor = 100;
-	const graphData = normalizedValues.map(value => { return {'btc': value * factor}});
+	const data = props.data;
 
-	let len = chg.length;
-	let percent = len >= 2 ? (parseFloat(chg[len-1] - chg[len-2]) * 100 / Math.abs(parseFloat(chg[len-1]))).toFixed(4) : 0;
-	// let percent = len > 0 ? parseFloat(chg[len-1]).toFixed(4) : 0;
+	let graph = props.dailyChange.slice(-10);
+	let len = props.dailyChange.length;
+	let percent = len == 0 ? 0: props.dailyChange[len-1];
+	const titles = [];
+	for (const entry of data) {
+		const key = Object.keys(entry)[0];
+		titles.push(key);
+	}
+	
+	const valueRange = Math.max(...graph) - Math.min(...graph);
+	const normalizedValues = graph.map(value => (value - Math.min(...graph)) / valueRange);
+	const factor = 100;
+	const graphData = normalizedValues.map(value => { return {'btc': value * factor}});	
+
+	let dec = props.name == 'forex' ? 4: 2;
 
 	return (
 		<div className='!mx-[5px] !m-auto !w-[310px] min-[390px]:!w-[342px] !h-[219px] border-white rounded-[6px] !bg-[#141217] min-[390px]:p-[10px] p-[7px] relative border '>
@@ -52,18 +60,8 @@ const AssetsItem = (props) => {
 										</div>
 										
 										<div className="flex items-center">
-											{
-												percent >=0 ? 
-												<>
-													<div className="w-0 h-0 border-[5px] border-b-[#00FF00] border-l-transparent border-r-transparent border-t-transparent mt-[-2.5px] ml-[15px]"></div>
-													<p className="text-[#00FF29] uppercase text-[18px] font-[400] leading-[34px]">{percent}%</p>
-												</>
-												:
-												<>
-													<div className="w-0 h-0 border-[5px] border-t-[#FF0000] border-l-transparent border-r-transparent border-b-transparent mb-[-9px] ml-[15px]"></div>
-													<p className="text-[red] uppercase text-[18px] font-[400] leading-[34px]">{percent}%</p>
-												</>
-											}
+											<div className={`${percent >= 0 ? 'border-b-[#00FF00] border-l-transparent border-r-transparent border-t-transparent mt-[-2.5px]': 'border-t-[#FF0000] border-l-transparent border-r-transparent border-b-transparent mb-[-9px]'} w-0 h-0 border-[5px] ml-[15px]`}></div>
+											<p className={`${percent >= 0 ? 'text-[#00FF29]' : 'text-[#FF0000]'} uppercase text-[18px] font-[400] leading-[34px]`}>{percent.toFixed(2)}%</p>
 										</div>
 									</div>
 
@@ -83,123 +81,104 @@ const AssetsItem = (props) => {
 						</div>
 					</div>
 
-					<div className="flex flex-row justify-between relative text-white" onClick={() => setIsFlip(!isFlip)}>
-
-						{
-							props.name == "crypto" ? 
-								<div>
-									<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PAIR</p>
-									<div className='flex items-center mb-1'>
-										<img src={btc} className='w-[20px] h-[20px]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">BTC/DAI</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={sol} className='w-[20px] h-[20px]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">SOL/DAI</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={eth} className='w-[20px] h-[20px]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">ETH/DAI</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={busd} className='w-[20px] h-[20px]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">BNB/DAI</p>
-									</div>
-								</div>
-							:	props.name == "forex" ? 
-								<div>
-									<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PAIR</p>
-									<div className='flex items-center mb-1'>
-										<img src={EUR} className='w-[20px] h-[20px] rounded-[50%]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">EUR/USD</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={AUD} className='w-[20px] h-[20px] rounded-[50%]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">AUD/USD</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={GBP} className='w-[20px] h-[20px] rounded-[50%]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">GBP/USD</p>
-									</div>
-									<div className='flex items-center mb-1'>
-										<img src={CNH} className='w-[20px] h-[20px] rounded-[50%]'></img>
-										<p className="leading-[34px] text-[14px] ml-[10px]">CNH/USD</p>
-									</div>
-								</div> :
+					<div className="relative text-white h-[199px]" onClick={() => setIsFlip(!isFlip)}>
+						{data.length > 0 ?
+							<div>
+							{
+								props.name == 'stocks' || props.name == 'defi' ?
 								(
-								<div className='w-full h-[199px]'>
-									<div className='flex justify-between flex-row '>
-										<div className='w-[52.89px]'>
+									<div>
+										<div className='flex'>
+											<div className='w-[80px]'>
+												<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PAIR</p>
+											</div>
+
+											<div className='w-[200px]'>
+												<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PRICE</p>
+											</div>
+
+											<div className='w-[80px]'>
+												<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">CHG</p>
+											</div>
+										</div>
+
+										<p className='uppercase text-[14px]'>Coming soon...</p>
+									</div>
+								) :
+								(
+									<div className='flex'>
+
+										<div className='w-[80px]'>
 											<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PAIR</p>
+											<div className='flex items-center mb-1'>
+												{
+													props.name == 'crypto' ? 
+														<img src={BTC} className='w-[20px] h-[20px]'></img>
+													: props.name == 'forex' ?
+														<img src={EUR} className='w-[20px] h-[20px] rounded-[50%]'></img>
+													: ''
+												}
+												<p className="leading-[34px] text-[14px] ml-[10px]">{titles[0]}</p>
+											</div>
+											<div className='flex items-center mb-1'>
+												{
+													props.name == 'crypto' ? 
+														<img src={SOL} className='w-[20px] h-[20px]'></img>
+													: props.name == 'forex' ?
+														<img src={AUD} className='w-[20px] h-[20px] rounded-[50%]'></img>
+													: ''
+												}
+												<p className="leading-[34px] text-[14px] ml-[10px]">{titles[1]}</p>
+											</div>
+											<div className='flex items-center mb-1'>
+												{
+													props.name == 'crypto' ? 
+														<img src={ETH} className='w-[20px] h-[20px]'></img>
+													: props.name == 'forex' ?
+														<img src={GBP} className='w-[20px] h-[20px] rounded-[50%]'></img>
+													: ''
+												}
+												<p className="leading-[34px] text-[14px] ml-[10px]">{titles[2]}</p>
+											</div>
+											<div className='flex items-center mb-1'>
+												{
+													props.name == 'crypto' ? 
+														<img src={BUSD} className='w-[20px] h-[20px]'></img>
+													: props.name == 'forex' ?
+														<img src={CNH} className='w-[20px] h-[20px] rounded-[50%]'></img>
+													: ''
+												}
+												<p className="leading-[34px] text-[14px] ml-[10px]">{titles[3]}</p>
+											</div>
 										</div>
-										<div className='w-[52.89px]'>
+
+										<div className='w-[200px]'>
 											<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PRICE</p>
+											<p className="leading-[34px] text-[14px] mb-1">{data[0][titles[0]].toFixed(dec)}</p>
+											<p className="leading-[34px] text-[14px] mb-1">{data[1][titles[1]].toFixed(dec)}</p>
+											<p className="leading-[34px] text-[14px] mb-1">{data[2][titles[2]].toFixed(dec)}</p>
+											<p className="leading-[34px] text-[14px] mb-1">{data[3][titles[3]].toFixed(dec)}</p>
 										</div>
-										<div className='w-[52.89px]'>
+
+										<div className='w-[80px]'>
 											<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">CHG</p>
+											<p className="leading-[34px] text-[14px] mb-1" style={data[0]['changes_24hrs'] >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{data[0]['changes_24hrs'] > 0 ? '+': ''}{data[0]['changes_24hrs'].toFixed(dec)}%</p>
+											<p className="leading-[34px] text-[14px] mb-1" style={data[1]['changes_24hrs'] >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{data[1]['changes_24hrs'] > 0 ? '+': ''}{data[1]['changes_24hrs'].toFixed(dec)}%</p>
+											<p className="leading-[34px] text-[14px] mb-1" style={data[2]['changes_24hrs'] >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{data[2]['changes_24hrs'] > 0 ? '+': ''}{data[2]['changes_24hrs'].toFixed(dec)}%</p>
+											<p className="leading-[34px] text-[14px] mb-1" style={data[3]['changes_24hrs'] >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{data[3]['changes_24hrs'] > 0 ? '+': ''}{data[3]['changes_24hrs'].toFixed(dec)}%</p>
 										</div>
-									</div>
-									<div>
-										<p className='uppercase text-[14px]'>Coming Soon...</p>
-									</div>
-								</div>
-							)
-						}
-						
-						{/* <div>
-							<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PRICE</p>
-						</div>
-						<div>
-							<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">CHG</p>
-						</div> */}
-							{
-								props.name == "crypto" && (
-									<div>
-										<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PRICE</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[0] != undefined ? parseFloat(props.data[0]["BTC/USD"]).toFixed(2) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[8] != undefined ? parseFloat(props.data[8]["SOL/USD"]).toFixed(2) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[1] != undefined ? parseFloat(props.data[1]["ETH/USD"]).toFixed(2) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[11] != undefined ? parseFloat(props.data[11]["DOT/USD"]).toFixed(2) : '0'}</p>
+									
 									</div>
 								)
 							}
-
-							{
-								props.name == "forex" && (
-									<div>
-										<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">PRICE</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[0] != undefined ? parseFloat(props.data[0]["EUR/USD"]).toFixed(4) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[1] != undefined ? parseFloat(props.data[1]["AUD/USD"]).toFixed(4) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[2] != undefined ? parseFloat(props.data[2]["GBP/USD"]).toFixed(4) : '0'}</p>
-										<p className="leading-[34px] text-[14px] mb-1">{props.data[3] != undefined ? parseFloat(props.data[3]["CNH/USD"]).toFixed(4) : '0'}</p>
-									</div>
-								)
-							}
+							</div>
 							
-							{
-								props.name == "crypto" && (
-									<div>
-										<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">CHG</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[0] != undefined &&  parseFloat(props.data[0]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[0] != undefined && parseFloat(props.data[0]["changes_24hrs"]) >=0 ? '+': ''}{props.data[0] != undefined ? parseFloat(props.data[0]["changes_24hrs"]).toFixed(2) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[8] != undefined &&  parseFloat(props.data[8]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[8] != undefined && parseFloat(props.data[8]["changes_24hrs"]) >=0 ? '+': ''}{props.data[8] != undefined ? parseFloat(props.data[8]["changes_24hrs"]).toFixed(2) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[1] != undefined &&  parseFloat(props.data[1]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[1] != undefined && parseFloat(props.data[1]["changes_24hrs"]) >=0 ? '+': ''}{props.data[1] != undefined ? parseFloat(props.data[1]["changes_24hrs"].toFixed(2)) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[11] != undefined &&  parseFloat(props.data[11]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[11] != undefined && parseFloat(props.data[11]["changes_24hrs"]) >=0 ? '+': ''}{props.data[11] != undefined ? parseFloat(props.data[11]["changes_24hrs"].toFixed(2)) : 0}%</p>
-									</div>
-								)
-							}
+							: 
 
-							{
-								props.name == "forex" && (
-									<div>
-										<p className="uppercase leading-[34px] font-bold text-[14px] mb-4">CHG</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[0] != undefined &&  parseFloat(props.data[0]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[0] != undefined && parseFloat(props.data[0]["changes_24hrs"]) >=0 ? '+': ''}{props.data[0] != undefined ? parseFloat(props.data[0]["changes_24hrs"]).toFixed(4) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[1] != undefined &&  parseFloat(props.data[1]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[1] != undefined && parseFloat(props.data[1]["changes_24hrs"]) >=0 ? '+': ''}{props.data[1] != undefined ? parseFloat(props.data[1]["changes_24hrs"]).toFixed(4) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[2] != undefined &&  parseFloat(props.data[2]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[2] != undefined && parseFloat(props.data[2]["changes_24hrs"]) >=0 ? '+': ''}{props.data[2] != undefined ? parseFloat(props.data[2]["changes_24hrs"].toFixed(4)) : 0}%</p>
-										<p className="leading-[34px] text-[14px] mb-1" style={props.data[3] != undefined &&  parseFloat(props.data[3]["changes_24hrs"]) >= 0 ? {color: '#00FF29'}: {color: '#FF0000'}}>{props.data[3] != undefined && parseFloat(props.data[3]["changes_24hrs"]) >=0 ? '+': ''}{props.data[3] != undefined ? parseFloat(props.data[3]["changes_24hrs"].toFixed(4)) : 0}%</p>
-									</div>
-								)
-							}
-						
+							<div className='flex items-center justify-center h-full'>
+								Loading...
+							</div>
+						}
 					</div>
 				</ReactCardFlip>
 			</div>
--- src/views/AssetsView.js
+++ src/views/AssetsView.js
@@ -1,41 +1,68 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import AssetsItem from '../components/AssetsItem';
 import axios from 'axios';
 
-let cryptoChg = [];
-let forexChg = [];
-let forexData = [];
+
+let cryptoGraphData = [];
+let forexGraphData = [];
 
 const AssetsView = () => {
-  const [ netType, setNetType ] = useState(['BTC', 'SOL', 'ETH', 'DOT']);
-  const [ cryptoData, setCryptoData ] = useState([]);
+  const [cryptoData, setCryptoData] = useState([]);
+  const [forexData, setForexData] = useState([]);
 
   useEffect(() => {
-    const myInterval = setInterval( (t)=> {
-      (async () => {
-        try{
-          const rep2 = await axios.get(`https://api2.aped.xyz/forex`);
-          forexData = rep2.data
-          // setForexData(rep2.data);
-  
-          const ch1 = await axios.get(`https://api2.aped.xyz/cryptos/dailychange`);
-          cryptoChg.push(ch1.data.cryptoDailyChange);
-          cryptoChg = cryptoChg.slice(-10);
-          const ch2 = await axios.get(`https://api2.aped.xyz/forex/dailychange`);
-          forexChg.push(ch2.data.forexDailyChange);
-          forexChg = forexChg.slice(-10);
-  
-          const rep1 = await axios.get(`https://api2.aped.xyz/cryptos`);
-          setCryptoData(rep1.data);
-        }
-        catch{
-          
-        }
-      
-      })();
-    }, 1000)
+    const fetchData = async () => {
+      try {
+        const [cryptoResponse, forexResponse, cryptoChangeResponse, forexChangeResponse] = await Promise.all([
+          axios.get(`https://api2.aped.xyz/cryptos`),
+          axios.get(`https://api2.aped.xyz/forex`),
+          axios.get(`https://api2.aped.xyz/cryptos/dailychange`),
+          axios.get(`https://api2.aped.xyz/forex/dailychange`)
+        ]);
 
+        const cryptoRes = cryptoResponse.data;
+        const forexRes = forexResponse.data;
+
+        cryptoGraphData.push(cryptoChangeResponse.data.cryptoDailyChange);
+        forexGraphData.push(forexChangeResponse.data.forexDailyChange);
+
+        setCryptoData([cryptoRes[0], cryptoRes[8], cryptoRes[1], cryptoRes[11]]);
+        setForexData([forexRes[0], forexRes[1], forexRes[2], forexRes[3]]);
+        
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // const fetchData1 = async () => {
+    //   try {
+    //     const [cryptoResponse, forexResponse, cryptoChangeResponse, forexChangeResponse] = await Promise.all([
+    //       axios.get(`https://api2.aped.xyz/cryptos`),
+    //       axios.get(`https://api2.aped.xyz/forex`),
+    //       axios.get(`https://api2.aped.xyz/cryptos/dailychange`),
+    //       axios.get(`https://api2.aped.xyz/forex/dailychange`)
+    //     ]);
+
+    //     const cryptoRes = cryptoResponse.data;
+    //     const forexRes = forexResponse.data;
+    //     const cryptoChangeRes = cryptoChangeResponse.data.cryptoDailyChange;
+    //     const forexChangeRes = forexChangeResponse.data.forexDailyChange;
+
+    //     setCryptoData([cryptoRes[0], cryptoRes[8], cryptoRes[1], cryptoRes[11]]);
+    //     setForexData([forexRes[0], forexRes[1], forexRes[2], forexRes[3]]);
+        
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+
+    const interval1 = setInterval(fetchData, 1000);
+    // const interval2 = setInterval(fetchData1, 3600000);
+    return () => {
+      clearInterval(interval1);
+      // clearInterval(interval2);
+    }
   }, []);
 
   const settings = {
@@ -80,30 +107,29 @@ const AssetsView = () => {
     ],
   };
 
-
   return (
-      <div className="assets flex mx-auto bg-black h-screen items-center justify-center overflow-hidden">
-        <div className="w-full">
-          <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase hidden md:block">Discover price feeds</p>
-          <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase block md:hidden ">Discover Tradable Assets</p>
-          <p className="mt-[5px] text-[#EDEDED] font-[300] text-[16px] leading-[17px] text-center uppercase block md:hidden ">Leverage trade crypto, stocks, and forex.</p>
+    <div className="assets flex mx-auto bg-black h-screen items-center justify-center overflow-hidden">
+      <div className="w-full">
+        <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase hidden md:block">Discover price feeds</p>
+        <p className="text-white font-bold text-[45px] leading-[45px] text-center uppercase block md:hidden ">Discover Tradable Assets</p>
+        <p className="mt-[5px] text-[#EDEDED] font-[300] text-[16px] leading-[17px] text-center uppercase block md:hidden ">Leverage trade crypto, stocks, and forex.</p>
 
-          <div className="border border-white w-[290px] min-[390px]:w-[327px] border-opacity-50 mx-auto mt-[10px] md:mt-[33px]"></div>
+        <div className="border border-white w-[290px] min-[390px]:w-[327px] border-opacity-50 mx-auto mt-[10px] md:mt-[33px]"></div>
 
-          <section className="center slider mt-[19px] flip-card">
-            <Slider {...settings}>
-              <AssetsItem name="crypto" data={cryptoData} dailyChange={cryptoChg}></AssetsItem>
-              <AssetsItem name="forex" data={forexData} dailyChange={forexChg}></AssetsItem>
-              <AssetsItem name="stocks" data={cryptoData} dailyChange={cryptoChg}></AssetsItem>
-              <AssetsItem name="defi" data={cryptoData} dailyChange={forexChg}></AssetsItem>
-            </Slider>
-          </section>
+        <section className="center slider mt-[19px] flip-card">
+          <Slider {...settings}>
+            <AssetsItem name="crypto" data={cryptoData} dailyChange={cryptoGraphData}></AssetsItem>
+            <AssetsItem name="forex" data={forexData} dailyChange={forexGraphData}></AssetsItem>
+            <AssetsItem name="stocks" data={cryptoData} dailyChange={cryptoGraphData}></AssetsItem>
+            <AssetsItem name="defi" data={cryptoData} dailyChange={forexGraphData}></AssetsItem>
+          </Slider>
+        </section>
 
-          <div className="border border-white w-[290px] min-[390px]:w-[327px] border-opacity-50 mx-auto mt-[19px] block md:hidden"></div>
+        <div className="border border-white w-[290px] min-[390px]:w-[327px] border-opacity-50 mx-auto mt-[19px] block md:hidden"></div>
 
-        </div>
       </div>
-  );
-}
+    </div>
+  )
+};
 
 export default AssetsView;
