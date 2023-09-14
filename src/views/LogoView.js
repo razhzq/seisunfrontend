87a1b141f746f49d6d2d6c47ea81f84752c527db
MC <55539221+sarisssa@users.noreply.github.com>
2023-09-14 12:02:15 -0700

    feat: add telegram link to mobile drop down menu

--- src/views/LogoView.js
+++ src/views/LogoView.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Slider from 'react-slick';
-import arbitrum from '../assets/Trade/arbitrum.svg';
-import polygon from '../assets/Trade/polygon.png';
-import zksync from '../assets/Trade/zksync.svg';
-import chainlink from '../assets/Trade/chainlink.svg';
-import marker from '../assets/Trade/marker.svg';
+import React from "react";
+import Slider from "react-slick";
+import arbitrum from "../assets/Trade/arbitrum.svg";
+import chainlink from "../assets/Trade/chainlink.svg";
+import marker from "../assets/Trade/marker.svg";
+import polygon from "../assets/Trade/polygon.png";
+import zksync from "../assets/Trade/zksync.svg";
 
 const LogoView = () => {
   const settings = {
     centerMode: true,
-    centerPadding: '10px',
+    centerPadding: "10px",
     slidesToShow: 5,
     speed: 500,
     slidesToScroll: 1,
     arrows: true,
     dots: false,
-		autoplay: true,
-		autoplayspeed: 1000,
+    autoplay: true,
+    autoplayspeed: 1000,
     // variableWidth: true,
     responsive: [
       {
@@ -55,16 +55,16 @@ const LogoView = () => {
       <section className="center slider">
         <div>
           <Slider {...settings}>
-						<img className="!w-[251.77px] !h-[51px]" src={arbitrum} alt="" />
-						<img className="!w-[186px] h-[39.11px]" src={polygon} alt="" />
-						<img className="!w-[196px] h-[38.32px]" src={zksync} alt="" />
-						<img className="!w-[191.53px] h-[58.84px]" src={chainlink} alt="" />
-						<img className="!w-[199px] h-[35.12px]" src={marker} alt="" />
-					</Slider>
+            <img className="!w-[251.77px] !h-[51px]" src={arbitrum} alt="" />
+            <img className="!w-[186px] h-[39.11px]" src={polygon} alt="" />
+            <img className="!w-[196px] h-[38.32px]" src={zksync} alt="" />
+            <img className="!w-[191.53px] h-[58.84px]" src={chainlink} alt="" />
+            <img className="!w-[199px] h-[35.12px]" src={marker} alt="" />
+          </Slider>
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default LogoView;
