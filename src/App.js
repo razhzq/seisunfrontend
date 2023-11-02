980fd97c011b35b7c98ca4903e0987ec06c49b3f
MC <55539221+sarisssa@users.noreply.github.com>
2023-11-01 17:31:37 -0700

    chore: various feedback changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import HeaderNav from './components/HeaderNav';
-import Home from './Home';
-import Contact from './views/ContactView';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import HeaderNav from "./components/HeaderNav";
+import Contact from "./views/ContactView";
 
-import './App.css';
 import "@fontsource/barlow";
-import "@fontsource/barlow/400.css";
 import "@fontsource/barlow/400-italic.css";
-import "@fontsource/barlow/700.css";
+import "@fontsource/barlow/400.css";
 import "@fontsource/barlow/700-italic.css";
-import "@fontsource/barlow/900.css";
+import "@fontsource/barlow/700.css";
 import "@fontsource/barlow/900-italic.css";
-import "slick-carousel/slick/slick.css";
+import "@fontsource/barlow/900.css";
 import "slick-carousel/slick/slick-theme.css";
-import { StickyContainer, Sticky } from 'react-sticky';
-import Footer from './Footer';
+import "slick-carousel/slick/slick.css";
+import "./App.css";
+import TermsofServiceScreen from "./components/TermsofServiceScreen";
+
+import Footer from "./Footer";
 
 function App() {
-
   return (
-      <div className="App">
-        <HeaderNav></HeaderNav>
+    <div className="App">
+      <HeaderNav></HeaderNav>
 
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="docs" element={<Home />} />
-            <Route path="contacts" element={<Contact />} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="docs" element={<Home />} />
+          <Route path="contacts" element={<Contact />} />
+          <Route path="terms-of-service" element={<TermsofServiceScreen />} />
+        </Routes>
+      </BrowserRouter>
 
-        <Footer></Footer>
-      </div>
+      <Footer></Footer>
+    </div>
   );
 }
 
